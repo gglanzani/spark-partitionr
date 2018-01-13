b1c29589d08ae6736f8c16b3f6ee9d08447e3264
Giovanni Lanzani <giovanni@lanzani.nl>
2018-01-13 09:35:53 +0100

    Add tests

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import shutil
+
+import pytest
+
+from spark_partitionr import create_spark_session
+
+
+@pytest.fixture(scope="session", autouse=True)
+def spark_session():
+    """
+    Fixture to create a SparkSession.
+
+    :returns: the Spark session
+    """
+    try:
+        shutil.rmtree('metastore_db')
+    except:
+        pass
+    spark = create_spark_session(database="test", table="run")
+    spark.sparkContext.setLogLevel('ERROR')
+    yield spark
+    spark.stop()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def sample_df(spark_session):
+    df = spark_session.read.json("tests/formatted-issue.json", multiLine=True)
+    return df
